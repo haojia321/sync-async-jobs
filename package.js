8497d36ae66d1ca060a8b78939588834bcd669b4
Hao <h1016j@gmail.com>
2018-02-21 15:43:44 -0500

    init

--- package.js
+++ package.js
@@ -1,24 +1,25 @@
 Package.describe({
-  name: 'sync-async-jobs',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+    name: 'haojia321:sync-async-jobs',
+    version: '0.0.1',
+    // Brief, one-line summary of the package.
+    summary: 'placeholder',
+    // URL to the Git repository containing the source code for this package.
+    git: 'https://github.com/haojia321/sync-async-jobs',
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.6.1');
-  api.use('ecmascript');
-  api.mainModule('sync-async-jobs.js');
+    api.versionsFrom('1.6.1');
+    api.use('ecmascript');
+    api.use('mongo');
+    api.mainModule('sync-async-jobs.js');
 });
 
 Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('sync-async-jobs');
-  api.mainModule('sync-async-jobs-tests.js');
+    api.use('ecmascript');
+    api.use('tinytest');
+    api.use('haojia321:sync-async-jobs');
+    api.mainModule('sync-async-jobs-tests.js');
 });
--- sync-async-jobs.js
+++ sync-async-jobs.js
@@ -3,3 +3,81 @@
 // Variables exported by this module can be imported by other packages and
 // applications. See sync-async-jobs-tests.js for an example of importing.
 export const name = 'sync-async-jobs';
+export const Jobs = {};
+let jobInfoMap = {};
+BackgroundJob = new Mongo.Collection('_BackgroundJob');
+
+Jobs.init = function(config) {
+    let maxJobCount = 5;
+    Meteor.setInterval(() => {
+        let runningJobs = BackgroundJob.find({ status: 'started' }).fetch();
+        let runningJobsCount = runningJobs.length;
+        console.log('A runningJobsCount: ', runningJobsCount);
+        for (let i = runningJobsCount; i < maxJobCount; i++) {
+            pickJob();
+        }
+    }, 5000);
+
+};
+
+Jobs.register = function(name, jobFunction, customJobCompletedPoint) {
+    jobInfoMap[name] = {
+        jobFunction: jobFunction,
+        customJobCompletedPoint: customJobCompletedPoint || false
+    };
+    console.log(jobInfoMap);
+};
+
+Jobs.run = function(obj) {
+    BackgroundJob.insert({
+        type: obj.type,
+        priority: obj.priority,
+        status: 'not_started',
+        created: new Date().getTime(),
+        arguments: obj.arguments
+    });
+};
+
+Jobs.markJobFinished = function(jobId) {
+    BackgroundJob.update({
+        _id: jobId
+    }, {
+        $set: {
+            status: 'success'
+        }
+    });
+};
+
+function pickJob() {
+    const job = BackgroundJob.find({
+        status: 'not_started'
+    }, {
+        sort: {
+            'created': 1
+        },
+        limit: 1
+    }).fetch()[0];
+    if (job) {
+        console.log('created job: ', job._id);
+        BackgroundJob.update({
+            _id: job._id
+        }, {
+            $set: {
+                'status': 'started'
+            }
+        });
+        var func = jobInfoMap[job.type].jobFunction;
+        var _arguments = job.arguments;
+        _arguments.push(job._id);
+        func.apply(null, _arguments);
+        if (!jobInfoMap[job.type].customJobCompletedPoint) {
+            BackgroundJob.update({
+                _id: job._id
+            }, {
+                $set: {
+                    status: 'success'
+                }
+            });
+        }
+    }
+}
