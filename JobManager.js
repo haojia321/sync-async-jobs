a5c2161217015044d6fe188b1c80df981eb035e2
Hao <h1016j@gmail.com>
2018-02-21 20:12:27 -0500

    fix

--- JobManager.js
+++ JobManager.js
@@ -1,75 +1,77 @@
-JobManager = { 'CONSTANT': constant };
-let jobInfoMap = {};
-JobManager.init = function(config) {
-    let maxJobCount = 5;
-    Meteor.setInterval(() => {
-        let runningJobs = _BackgroundJob.find({ status: constant['STATUS_STARTED'] }).fetch();
-        let runningJobsCount = runningJobs.length;
-        console.log('Running Jobs Count: ', runningJobsCount);
-        for (let i = runningJobsCount; i < maxJobCount; i++) {
-            pickJob();
-        }
-    }, 5000);
+if (Meteor.isServer) {
+    JobManager = { 'CONSTANT': constant };
+    let jobInfoMap = {};
+    JobManager.init = function(config) {
+        let maxJobCount = 5;
+        Meteor.setInterval(() => {
+            let runningJobs = _BackgroundJob.find({ status: constant['STATUS_STARTED'] }).fetch();
+            let runningJobsCount = runningJobs.length;
+            console.log('Running Jobs Count: ', runningJobsCount);
+            for (let i = runningJobsCount; i < maxJobCount; i++) {
+                pickJob();
+            }
+        }, 5000);
 
-};
-
-JobManager.register = function(name, jobFunction, customJobCompletedPoint) {
-    jobInfoMap[name] = {
-        jobFunction: jobFunction,
-        customJobCompletedPoint: customJobCompletedPoint || false
     };
-};
 
-JobManager.run = function(obj) {
-    _BackgroundJob.insert({
-        type: obj.type,
-        priority: obj.priority,
-        status: constant['STATUS_NOT_STARTED'],
-        created: new Date(),
-        arguments: obj.arguments
-    });
-};
+    JobManager.register = function(name, jobFunction, customJobCompletedPoint) {
+        jobInfoMap[name] = {
+            jobFunction: jobFunction,
+            customJobCompletedPoint: customJobCompletedPoint || false
+        };
+    };
 
-JobManager.markJobFinished = function(jobId) {
-    _BackgroundJob.update({
-        _id: jobId
-    }, {
-        $set: {
-            status: constant['STATUS_SUCCESS']
-        }
-    });
-};
+    JobManager.run = function(obj) {
+        _BackgroundJob.insert({
+            type: obj.type,
+            priority: obj.priority,
+            status: constant['STATUS_NOT_STARTED'],
+            created: new Date(),
+            arguments: obj.arguments
+        });
+    };
 
-function pickJob() {
-    const job = _BackgroundJob.find({
-        status: constant['STATUS_NOT_STARTED']
-    }, {
-        sort: {
-            'created': 1
-        },
-        limit: 1
-    }).fetch()[0];
-    if (job) {
-        console.log('created job: ', job._id);
+    JobManager.markJobFinished = function(jobId) {
         _BackgroundJob.update({
-            _id: job._id
+            _id: jobId
         }, {
             $set: {
-                'status': constant['STATUS_STARTED']
+                status: constant['STATUS_SUCCESS']
             }
         });
-        var func = jobInfoMap[job.type].jobFunction;
-        var _arguments = job.arguments;
-        _arguments.push(job._id);
-        func.apply(null, _arguments);
-        if (!jobInfoMap[job.type].customJobCompletedPoint) {
+    };
+
+    function pickJob() {
+        const job = _BackgroundJob.find({
+            status: constant['STATUS_NOT_STARTED']
+        }, {
+            sort: {
+                'created': 1
+            },
+            limit: 1
+        }).fetch()[0];
+        if (job) {
+            console.log('created job: ', job._id);
             _BackgroundJob.update({
                 _id: job._id
             }, {
                 $set: {
-                    status: constant['STATUS_SUCCESS']
+                    'status': constant['STATUS_STARTED']
                 }
             });
+            var func = jobInfoMap[job.type].jobFunction;
+            var _arguments = job.arguments;
+            _arguments.push(job._id);
+            func.apply(null, _arguments);
+            if (!jobInfoMap[job.type].customJobCompletedPoint) {
+                _BackgroundJob.update({
+                    _id: job._id
+                }, {
+                    $set: {
+                        status: constant['STATUS_SUCCESS']
+                    }
+                });
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-test
+* Run up to 5 jobs at same time.
+* Check poll in every 5 seconds.
+
+TODO
+* Config run up to X jobs at same time.
+* Config check poll in every X seconds.
+* Config run a job in future time by seconds.
+* Config job due time.
+* Config job priority.
